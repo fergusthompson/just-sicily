b55fd4257eb66bc3264b3e91d8cfba33f11f2353
fergusthompson <fergus.thompson@hotmail.com>
2020-05-08 08:52:42 +0000

    added resturants section and fiddled with js

---
+++ assets/javascript/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function() { 
+                 $(".link-underline").hover(function() { 
+                $(this).css("width", "300").css("height", "150"); 
+            }); 
+        }); 
