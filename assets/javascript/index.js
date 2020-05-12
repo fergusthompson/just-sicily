03431978e24fc94d27f64f0e1c870a76137cae3e
fergusthompson <fergus.thompson@hotmail.com>
2020-05-12 09:08:31 +0000

    added jquery for animations on page

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,5 +1,62 @@
-$(document).ready(function() { 
-                 $(".link-underline").hover(function() { 
-                $(this).css("width", "300").css("height", "150"); 
-            }); 
-        }); 
+
+  $(".see-more-link").hover(function(){
+      console.log("connected");
+    $(".see-link-underline").animate({
+    width: "70%",
+    marginLeft: "0.6in",
+    fontSize: "3em",
+    borderWidth: "10px"
+  }, 1500 );
+  });
+
+  $(".book-now-link").hover(function(){
+      console.log("connected");
+    $(".book-link-underline").animate({
+    width: "70%",
+    marginLeft: "0.6in",
+    fontSize: "3em",
+    borderWidth: "10px"
+  }, 1500 );
+  });
+
+  $(".reserve-link").hover(function(){
+      console.log("connected");
+    $(".reserve-link-underline").animate({
+    width: "70%",
+    marginLeft: "0.6in",
+    fontSize: "3em",
+    borderWidth: "10px"
+  }, 1500 );
+  });
+
+  $( window ).scroll(function() {
+  $( ".about-header-underline" ).animate({
+      width: "70%",
+    marginLeft: "0.6in",
+    fontSize: "3em",
+    borderWidth: "10px"
+
+  }, 4000);
+});
+
+$( window ).scroll(function() {
+  $( ".hotels-header-underline" ).animate({
+      width: "70%",
+    marginLeft: "0.6in",
+    fontSize: "3em",
+    borderWidth: "10px"
+
+  }, 4000);
+});
+
+$( window ).scroll(function() {
+  $( ".dining-header-underline" ).animate({
+      width: "70%",
+    marginLeft: "0.6in",
+    fontSize: "3em",
+    borderWidth: "10px"
+
+  }, 4000);
+});
+
+
