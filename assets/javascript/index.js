eb5646bac5ecfc52e0da44c476f492d9c25553a2
fergusthompson <fergus.thompson@hotmail.com>
2020-06-06 09:25:51 +0000

    added menu overlay

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -59,4 +59,14 @@ $( window ).scroll(function() {
   }, 4000);
 });
 
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+} 
+
 
