bfcc7c6d39f50c6a5071e7c8a807c7b7cf4fe11a
fergusthompson <fergus.thompson@hotmail.com>
2020-06-08 09:48:39 +0000

    added hotle page and changed booking form

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -69,4 +69,14 @@ function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 } 
 
+/* Open when someone clicks on the span element */
+function openNav2() {
+  document.getElementById("myNav2").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav2() {
+  document.getElementById("myNav2").style.width = "0%";
+} 
+
 
